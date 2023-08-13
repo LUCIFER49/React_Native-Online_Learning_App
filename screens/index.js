2fb4ed1fbc8b9f9ac59789651e651da0857b8d6a
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-13 21:52:30 +0530

    start of the journey

---
+++ screens/index.js
@@ -0,0 +1,8 @@
+import MainLayout from "./Dashboard/MainLayout";
+import Home from "./Dashboard/Home";
+import Profile from "./Dashboard/Profile";
+import Search from "./Dashboard/Search";
+
+export {
+    MainLayout, Home, Profile, Search,
+}
